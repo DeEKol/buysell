9a7550036cd05541aa8a595d2cadf1be60c3221e
dekol <dekolodkin@mail.com>
2022-08-29 17:39:45 +0300

    installation app, db; test controller; add ProductEntity, ProductTypeEntity

---
+++ src/main/java/com/deekol/buysell/domain/ProductEntity.java
@@ -0,0 +1,43 @@
+package com.deekol.buysell.domain;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "product")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@ManyToOne
+	@JoinColumn(name = "product_type_id")
+	private ProductTypeEntity productTypeEntity;
+	
+	private String name;
+	private String descriprion;
+	
+	private LocalDate dateBuy;
+	private LocalDate dateSold;
+	
+	private BigDecimal priceBuy;
+	private BigDecimal priceSell;
+	private BigDecimal cost;
+}
---
+++ src/main/java/com/deekol/buysell/domain/ProductTypeEntity.java
@@ -0,0 +1,33 @@
+package com.deekol.buysell.domain;
+
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "product_type")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductTypeEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	private String description;
+	
+	@OneToMany(mappedBy = "productTypeEntity")
+	private Set<ProductEntity> productEntity;
+}
